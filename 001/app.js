54a58de9ab3f1733d7d7af2e0fecfd0c9bd0afda
kalgo4444 <abuxyziabd@gmail.com>
2025-04-12 22:26:10 +0500

    first

---
+++ 001/app.js
@@ -0,0 +1,97 @@
+{
+    // 1 - misol 
+    // var a = prompt("A = ");
+    // console.log("P = " + 4 * Number(a));
+}
+{
+    // 2 - misol
+    // var a = prompt("A = ");
+    // console.log("S = " + Number(a) ** 2); 
+}
+{
+    // 3 - misol
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // console.log("S = " + Number(a) * Number(b));
+    // console.log("P = " + 2 * (Number(a) + Number(b)))
+}
+{
+    // 4 - misol
+    // var d = prompt("d = ");
+    // console.log("L = " + Math.PI * d);
+}
+{
+    // 5 - misol
+    // var a = prompt("a = ");
+    // console.log("V = " + Number(a) ** 3);
+    // console.log("S = " + 6 * (Number(a) ** 2));
+}
+{
+    // 6 - misol
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // var c = prompt("C = ");
+    // a = Number(a)
+    // b = Number(b)
+    // c = Number(c)
+    // console.log("V = " + a * b * c);
+    // console.log("S = " + 2 * (a * b + b * c + a * c));
+}
+{
+    // 7 - misol
+    // var r = prompt("R = ");
+    // var s = prompt("S = ");
+    // r = Number(r)
+    // s = Number(s)
+    // console.log("L = " + 2 * Math.PI * r);
+    // console.log("S = " + Math.PI * Math.pow(r,2));
+}
+{
+    // 8 - misol 
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // a = Number(a)
+    // b = Number(b)
+    // console.log((a + b) / 2);
+}
+{
+    // 9 - misol
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // a = Number(a)
+    // b = Number(b)
+    // console.log(Math.sqrt(a * b));
+}
+{
+    // 10 - misol 
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // a = Number(a)
+    // b = Number(b)
+    // console.log("a + b = " + (a + b));
+    // console.log("a * b = " + a * b);
+    // console.log("a^ = " + Math.pow(a,2));
+    // console.log("b^ = " + Math.pow(b,2));
+}
+{
+    // 11 - misol
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // a = Number(a)
+    // b = Number(b)
+    // console.log("a + b = " + (a + b));
+    // console.log("a * b = " + a * b);
+    // console.log("moduli = " + Math.abs(a));
+    // console.log("moduli = " + Math.abs(b));
+}
+{
+    // 12 - misol
+    var a = prompt("A = ");
+    var b = prompt("B = ");
+    a = Number(a)
+    b = Number(b)
+    var c = Math.sqrt((a ** 2) + (b ** 2))
+    c = Number(c)
+    console.log("c = " + c);
+    console.log("P = " + (a + b + c));
+}
