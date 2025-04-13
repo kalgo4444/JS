fc78b94a4ec0015f95687aecba32fc135f86b09e
kalgo4444 <abuxyziabd@gmail.com>
2025-04-13 10:55:36 +0500

    www

--- 001/app.js
+++ 001/app.js
@@ -86,12 +86,84 @@
 }
 {
     // 12 - misol
-    var a = prompt("A = ");
-    var b = prompt("B = ");
-    a = Number(a)
-    b = Number(b)
-    var c = Math.sqrt((a ** 2) + (b ** 2))
-    c = Number(c)
-    console.log("c = " + c);
-    console.log("P = " + (a + b + c));
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // a = Number(a)
+    // b = Number(b)
+    // var c = Math.sqrt((a ** 2) + (b ** 2))
+    // c = Number(c)
+    // console.log("c = " + c);
+    // console.log("P = " + (a + b + c));
+}
+{
+    // 13 - misol
+    // var r1 = prompt("R1 (R1 < R2) = ");
+    // var r2 = prompt("R2 (R1 < R2) = ");
+    // r1 = Number(r1)
+    // r2 = Number(r2)
+    // console.log("S1 = " + Math.PI * r1);
+    // console.log("S2 = " + Math.PI * r2);
+    // console.log("S3 = " + Math.PI * (r1 - r2));
+}
+{
+    // 14 - masala
+    // var r = prompt("R = ")
+    // r = Number(r)
+    // console.log("L = " + 2 * Math.PI * r);
+    // console.log("S = " + Math.PI * Math.pow(r,2));
+}
+{
+    // 17 - masala 
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // var c = prompt("C = ");
+    // a = Number(a)
+    // b = Number(b)
+    // c = Number(c)
+    // var ac = a + c;
+    // var bc = b + c;
+    // console.log("AC = " + ac);
+    // console.log("BC = " + bc);
+    // console.log("Yigindisi = " + (ac + bc));
+}
+{
+    // 18 - masala
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // var c = prompt("C = ");
+    // a = Number(a)
+    // b = Number(b)
+    // c = Number(c)
+    // var ac = a * c;
+    // var bc = b * c;
+    // console.log("AC = " + ac);
+    // console.log("BC = " + bc);
+}
+{
+    // 19 - masala
+    // var a = prompt("A = ");
+    // var b = prompt("B = ");
+    // var c = prompt("C = ");
+    // var d = prompt("D = ");
+    // a = Number(a)
+    // b = Number(b)
+    // c = Number(c)
+    // d = Number(d)
+    // console.log("P = " + (a + b + c + d));
+    // console.log("S = " + (a * b * c * d));
+}
+{
+    // 20 - masala 
+    // var x1 = prompt("X1 (x1 = y1) = ");
+    // var y1 = prompt("Y1 (x1 = y1) = ");
+    // var x2 = prompt("X2 (x2 = y2) = ");
+    // var y2 = prompt("Y2 (x2 = y2) = ");
+    // x1 = Number(x1)
+    // y1 = Number(y1)
+    // x2 = Number(x2)
+    // y2 = Number(y2)
+    // console.log("Masofa = " + Math.sqrt((Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2))));
+}
+{
+
 }
---
+++ test/js.js
@@ -0,0 +1,2 @@
+var a = prompt("Name: ");
+alert("Hi " + a);
