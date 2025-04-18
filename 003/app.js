3cedb2a0598a3c1b971b8d8e511d4d969aedecac
kalgo4444 <abuxyziabd@gmail.com>
2025-04-19 01:10:07 +0500

    10 full

---
+++ 003/app.js
@@ -0,0 +1,148 @@
+{
+    // 1 - masala
+    // let num = 10;
+    // if (num > 0){
+    //     console.log(num + 1);
+    // } else {
+    //     console.log(num);
+        
+    // }
+}
+{
+    // 2 - masala 
+    // let num = -10;
+    // if (num > 0){
+    //     console.log(num + 1);
+    // } else {
+    //     console.log(num + 2);
+    // }
+}
+{
+    // 3 - masala 
+    // let num = 0;
+    // if (num > 0) {
+    //     console.log(num + 1);
+    // } else if (num < 0) {
+    //     console.log(num * 2);
+    // } else {
+    //     console.log(num + 10);
+    // }
+}
+{
+    // 4 - masala 
+    // let num1 = 3;
+    // let num2 = 4;
+    // let num3 = 5;
+    // let r = null;
+    // if (num1 > 0) {
+    //     r = r + 1
+    // }
+    // if (num2 > 0) {
+    //     r = r + 1
+    // }
+    // if (num3 > 0) {
+    //     r = r + 1
+    // }
+    // console.log(r);
+}
+{
+    // 5 - masala 
+    // let num1 = 3;
+    // let num2 = -4;
+    // let num3 = -5;
+    // let r = null;
+    // let r1 = null
+    // if (num1 > 0) {
+    //     r = r + 1
+    // } else {
+    //     r1 = r1 + 1
+    // }
+    // if (num2 > 0) {
+    //     r = r + 1
+    // } else {
+    //     r1 = r1 + 1
+    // }
+    // if (num3 > 0) {
+    //     r = r + 1
+    // } else {
+    //     r1 = r1 + 1
+    // }
+    // console.log(`${r} musbat`);
+    // console.log(`${r1} manfiy`);
+}
+{
+    // 6 - masala
+    // let num1 = 3;
+    // let num2 = 4;
+    // let r = Math.max(num1, num2);
+    // console.log(r);
+
+    // let num1 = 3, num2 = 4, r = Math.max(num1, num2);
+    // console.log(r);
+}
+{
+    // 7 - masala 
+    // let num1 = 5;
+    // let num2 = 4;
+    // if (num1 < num2){
+    //     console.log("1");
+    // } else if (num2 < num1 ) {
+    //     console.log("2");
+    // } else {
+    //     console.log("TENG!");
+    // }
+}
+{
+    // 8 - masala 
+    // let num1 = 4;
+    // let num2 = 5;
+    // if (num1 > num2) {
+    //     console.log(num1);
+    //     console.log(num2);
+    // } else if (num2 > num1){
+    //     console.log(num2);
+    //     console.log(num1);
+    // } else {
+    //     console.log("teng");
+    // }
+}
+{
+    // 9 - masala
+    // let a = 12;
+    // let b = 10;
+    // let c = null;
+    // if (a < b) {
+    //     console.log(a + b);
+    // } else if (a > b) {
+    //     c = a
+    //     a = b
+    //     b = c
+    //     console.log(`a = ${a}`);
+    //     console.log(`b = ${b}`);
+    //     console.log(`${a} + ${b} = ${a + b}`);
+    // } else {
+    //     console.log("teng");
+    // }
+}
+{
+    // 10 - masala 
+    // let a = 5;
+    // let b = 5;
+    // let c = null;
+    // if (a == b){
+    //     c = a + b;
+    //     a = c
+    //     b = c
+    //     console.log(`a = ${a}`);
+    //     console.log(`b = ${b}`);
+    // } else if (a != b) {
+    //     a = 0
+    //     b = 0
+    //     console.log(`a = ${a}`);
+    //     console.log(`b = ${b}`);
+
+    // } else {
+    //     console.log("error");
+        
+    // }
+}
